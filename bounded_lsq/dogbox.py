BUG: Fixed bug with scaling in dogbox.py

bounded_lsq/dogbox.py
@@ -130,10 +130,7 @@ def dogbox(fun, jac, x0, lb, ub, ftol, xtol, gtol, max_nfev, scaling):
         J_norm[J_norm == 0] = 1
         scale = 1 / J_norm
     else:
-        scale = 1 / np.asarray(scaling)
-
-    if scale.ndim == 0:
-        scale = np.resize(scale, x0.shape)
+        scale = 1 / scaling
 
     Delta = np.linalg.norm(x0 / scale, ord=np.inf)
     if Delta == 0:
@@ -154,7 +151,7 @@ def dogbox(fun, jac, x0, lb, ub, ftol, xtol, gtol, max_nfev, scaling):
     while nfev < max_nfev:
         g = J.T.dot(f)
 
-        if scaling == 'auto':
+        if scaling == 'jac':
             J_norm = np.linalg.norm(J, axis=0)
             with np.errstate(divide='ignore'):
                 scale = np.minimum(scale, 1 / J_norm)